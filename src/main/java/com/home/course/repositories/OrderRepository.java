1460f28545ac25e94995f6cf17c3f800ba345855
phb2000 <p.becker2000@hotmail.com>
2020-06-24 17:40:39 -0300

    Order

---
+++ src/main/java/com/home/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.home.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.home.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
