62a57470cc3e0f55d4f714df23972799b16c59f3
phb2000 <p.becker2000@hotmail.com>
2020-06-26 17:05:20 -0300

    Product

---
+++ src/main/java/com/home/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.home.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.home.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
